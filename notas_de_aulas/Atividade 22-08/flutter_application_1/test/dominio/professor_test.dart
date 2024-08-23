ec99e635acbcb9ffbca7b387193dac4bdf687449
willzin454 <williamcarvalho493@gmail.com>
2024-08-22 21:28:26 -0300

    Aulas

--- notas_de_aulas/Atividade 22-08/flutter_application_1/test/dominio/professor_test.dart
+++ notas_de_aulas/Atividade 22-08/flutter_application_1/test/dominio/professor_test.dart
@@ -1,3 +1,16 @@
-main(){
-  
+import 'package:flutter_application_1/dominio/dto/dto_professor.dart';
+import 'package:flutter_application_1/dominio/professor.dart';
+import 'package:flutter_application_1/dominio/interface/i_dao_professor.dart';
+
+main() {
+  var dto = DTOProfessor(nome: 'João', cpf: '100.111.111-74');
+  var dao = DAOProfessorFake();
+  var professor = Professor(dto: dto, dao: dao);
+
 }
+  class DAOProfessorFake implements IDAOProfessor{
+    @override
+    DTOProfessor salvar(DTOProfessor dto){
+      return dto;
+    }
+  }
